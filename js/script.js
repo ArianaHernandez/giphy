8db6efb7fc71c21a6dfc2818fbf08a94505f3e32
Ariana Hernandez <arianahernandez@whsat.org>
2019-03-14 22:25:03 +0000

    Starter

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+var theUrl="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 $("#search-button").click(function(){
-  
-  
-  
+  $.ajax({
+  url: theUrl,
+method: "GET",
+  success: function(response) {
+    $("body").append("<img src='"+response.data[0].images+"'/>");
+      
+  },
+});
 });
 
