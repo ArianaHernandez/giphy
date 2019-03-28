b812fdc8e28ccf550e7eef80732ce13eaaae88d8
Ariana Hernandez <arianahernandez@whsat.org>
2019-03-28 22:00:05 +0000

    Costumized

--- js/script.js
+++ js/script.js
@@ -2,15 +2,32 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-var theUrl="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
-$("#search-button").click(function(){
-  $.ajax({
-  url: theUrl,
-method: "GET",
-  success: function(response) {
-    $(".container").append("<img src='"+response.data[0].images.fixed_width.url+"'/>");
-      
-  },
+$( document ).ready(function() {
+   $(".text-center").show();
 });
+$("#search-button").click(function(){
+    var searchTerm = $("#search-term").val();
+var theUrl="https://api.giphy.com/v1/gifs/search?q="+searchTerm+"&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+    url: theUrl,
+    method: "GET",
+    success: function(response) {
+        $(".giffy").append("<img src='"+response.data[0].images.fixed_width.url+"'/>");
+      $(".text-center").hide();
+    },
+    });
 });
 
+$("#random-button").click(function(){
+    var searchTerm = $("#search-term").val();
+    var num= Math.floor(Math.random() * 30);
+var theUrl="https://api.giphy.com/v1/gifs/search?q="+searchTerm+"&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+    url: theUrl,
+    method: "GET",
+    success: function(response) {
+        $(".giffy").append("<img src='"+response.data[num].images.fixed_width.url+"'/>");
+       $(".text-center").hide();
+    },
+    });
+});
